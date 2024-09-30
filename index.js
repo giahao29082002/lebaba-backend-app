ee53354d41f50018945419d8c05e3f87cf369f7c
giahao29082002 <nguyengiahao10a3@gmail.com>
2024-10-01 00:41:45 +0700

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'https://lebaba-frontend-final.vercel.app',
+    origin: 'https://lebaba-frontend-final-orpin.vercel.app',
     credentials: true
 }))
 
